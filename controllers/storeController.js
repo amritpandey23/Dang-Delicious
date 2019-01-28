5379db495895642c6f0660bc5f4324abb6aff9da
ap4gh <pandey.amrit@outlook.com>
2019-01-28 17:44:59 +0530

    The addstore form now can save data to the database

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose')
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
     res.render('index')
 }
@@ -6,6 +9,9 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Stores' })
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body)
+    await store.save()
+    console.log('Data saved successfully.')
+    res.redirect('/')
 }
