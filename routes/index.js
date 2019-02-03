cfa1d87cdfaf20fd97728b275d81b0303a9038dc
A. Pandey <pandey.amrit@outlook.com>
2019-02-03 14:57:47 +0530

    store add and edit route

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,7 @@ router.get('/', storeController.homePage)
 router.get('/add', storeController.addStore)
 router.post('/add', catchErrors(storeController.createStore))
 router.get('/stores', catchErrors(storeController.getStores))
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+router.post('/add/:id', catchErrors(storeController.updateStore))
 
 module.exports = router
