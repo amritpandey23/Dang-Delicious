637a240793d637892f5e6d499673b13fcdea0c64
ap4gh <pandey.amrit@outlook.com>
2019-02-09 13:13:07 +0530

    prettier format

--- models/store.js
+++ models/store.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose')
-const slugify = require('slugify')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please enter a store name'
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
     },
-    slug: String,
-    description: {
-        type: String,
-        trim: true
-    },
-    tags: [String],
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    location: {
-        type: {
-            type: String,
-            default: 'Point'
-        },
-        coordinates: [{
-            type: Number,
-            required: 'You need to supply coordinates'
-        }],
-        address: {
-            type: String,
-            required: 'You need to add an address'
-        }
-    },
-    photo: String
+    coordinates: [
+      {
+        type: Number,
+        required: 'You need to supply coordinates'
+      }
+    ],
+    address: {
+      type: String,
+      required: 'You need to add an address'
+    }
+  },
+  photo: String
 });
 
 storeSchema.pre('save', async function(next) {
@@ -45,9 +47,4 @@ storeSchema.pre('save', async function(next) {
   next();
 });
 
-storeSchema.pre('save', function(next) {
-    this.slug = slugify(this.name)
-    next()
-})
-
-module.exports = storeSchema
+module.exports = storeSchema;
