0dd4ba94b324752499e1eb57cfa9350dbf1d8825
A. Pandey <pandey.amrit@outlook.com>
2019-02-27 21:42:42 +0530

    handled login and register route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -27,4 +28,7 @@ router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoreByTags));
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTags));
 
+router.get('/login', catchErrors(userController.loginForm));
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
 module.exports = router;
