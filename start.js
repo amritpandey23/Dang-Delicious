f3e314c6cdd99705bd40e51001c8a6262fc883d8
A. Pandey <pandey.amrit@outlook.com>
2019-02-27 21:32:32 +0530

    imported user model

--- start.js
+++ start.js
@@ -1,36 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 // Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split('.').map(parseFloat)
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 7 || (major === 7 && minor <= 5)) {
   console.log(
     "🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou're on an older version of node that doesn't support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n "
-  )
-  process.exit()
+  );
+  process.exit();
 }
 
 // import environmental variables from our variables.env file
 require('dotenv').config({
   path: 'variables.env'
-})
+});
 
 // Connect to our Database and handle any bad connections
 
-mongoose.connect(
-  process.env.DATABASE,
-  { useNewUrlParser: true },
-  err => {
-    if (err) {
-      console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err}`)
-    }
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }, err => {
+  if (err) {
+    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err}`);
   }
-)
+});
 // Setting models via mongoose
-mongoose.model('Store', require('./models/store'))
+mongoose.model('Store', require('./models/store'));
+mongoose.model('User', require('./models/user'));
 
 // Start our app!
-const app = require('./app')
-app.set('port', process.env.PORT || 7777)
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`)
-})
+  console.log(`Express running → PORT ${server.address().port}`);
+});
